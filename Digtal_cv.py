8ebe73fce103fdb2bb549c1b3a2118d301834b3e
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-10-09 13:59:45 +0300

    Update and rename Unconfirmed 171622.crdownload to  Digtal_cv.py

---
+++ Digtal_cv.py
@@ -0,0 +1,116 @@
+from pathlib import Path
+
+import streamlit as st
+from PIL import Image
+
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "Salwa ML.pdf"
+profile_pic = current_dir / "assets" / "300846713_5028669960567203_6940056269635120261_n.jpg"
+
+
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Digital CV | Salwa Mohammed"
+PAGE_ICON = ":wave:"
+NAME = "John Doe"
+DESCRIPTION = """
+ML engineer, image processing , Comuter vision.
+"""
+EMAIL = "salwa.studies@gmail.com"
+SOCIAL_MEDIA = {
+  
+    "LinkedIn": "https://www.linkedin.com/in/salwa-muhammed-53b873246/",
+    "GitHub": "https://github.com/salwamohammed23",
+   
+}
+#PROJECTS = {}
+
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",
+    )
+    st.write("📫", EMAIL)
+
+
+# --- SOCIAL LINKS ---
+st.write('\n')
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write('\n')
+st.subheader("Experience & Qulifications")
+st.write(
+    """
+- ✔️ Training AI atZewail City of Science and Technology
+- ✔️  Internship Trainee at WorldQuant University
+- ✔️ teacher of Mathematics
+
+"""
+)
+
+
+# --- SKILLS ---
+st.write('\n')
+st.subheader("Hard Skills")
+st.write(
+    """
+- 👩‍💻 Programming: Python (Scikit-learn, Pandas,TensorFlow, Keras,streamlit ), SQL, VBA
+- Write paper in Latic
+
+- 📊 Data Visulization:Plotly,, Statistics
+- 📚 Modeling: ML models, DL models
+- 🗄️ Databases: MongoDB, MySQL
+"""
+)
+
+
+# --- WORK HISTORY ---
+st.write('\n')
+st.subheader("Work History")
+st.write("---")
+
+# --- JOB 1
+st.write("Mathematics Teacher")
+
+
+
+
+# --- Education ---
+st.write('\n')
+st.subheader("Educationy")
+st.write("---")
+
+# --- Edu 1
+st.write("M.Sc. degree in ‘pure Mathematics’.2017, Faculty of Science, 
+South Valley University.")
+# --- Edu 2
+st.write("Bachelor Degree in Science (Mathematics) 2008, Al-Azhar University / Faculty of Science.")
+# --- Edu 3
+st.write("Professional Diploma in Software Engineering with Distinction, Cairo University with Honors .")
